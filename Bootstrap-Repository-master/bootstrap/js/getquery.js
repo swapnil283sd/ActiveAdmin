9cc1c988db5123f7ce75600987ecbfdb1bf2584a
DIPALIBANKAR <dipaliexpert05@gmail.com>
2018-03-28 17:47:58 +0530

    DIPALIBANKAR
    getquery added

---
+++ Bootstrap-Repository-master/bootstrap/js/getquery.js
@@ -0,0 +1,14 @@
+
+<head>
+<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/index.css">
+        <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+<script>
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+</script>
+</head>
