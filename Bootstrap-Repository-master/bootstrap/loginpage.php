92f9c9d23acb0673cbd47f57c0e7f32e404b9e04
Umesh Charpe <umeshexpert05@gmail.com>
2018-03-29 14:35:33 +0530

    Umesh
    Update loginpage and querie_generate.php created.

--- Bootstrap-Repository-master/bootstrap/loginpage.php
+++ Bootstrap-Repository-master/bootstrap/loginpage.php
@@ -1,3 +1,7 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,68 +10,79 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/loginpage.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="fonts/css/font-awesome.min.css">
-	<link href="https://fonts.googleapis.com/css?family=Quicksand|Roboto" rel="stylesheet">  
+	<link href="https://fonts.googleapis.com/css?family=Quicksand|Roboto" rel="stylesheet">
+	
+	<script type="text/javascript">
+	 
+		function jsfunction(){
+				document.getElementById("login_failed").style.visibility = "visible";
+		}
+	</script>
+
 </head>
 
 <body>
 	<?php
-		include_once 'backend/dbconnect.php';
-		$emp_username=$_POST['emp_username'];
-		$emp_password=$_POST['emp_password'];
+		if(!empty($_POST)){
+			include_once 'backend/dbconnect.php';
+			$emp_username=$_POST['emp_username'];
+			$emp_password=$_POST['emp_password'];
 
-		$sql = "SELECT * FROM employee where emp_name='$emp_username' and emp_password='$emp_password'";
-		$result = mysqli_query($conn, $sql);
+			$sql = "SELECT * FROM employee where emp_name='$emp_username' and emp_password='$emp_password'";
+			$result = mysqli_query($conn, $sql);
 		
-		if (mysqli_num_rows($result) > 0) {
-			// output data of each row
-			while($row = mysqli_fetch_assoc($result)) {
-			/*
-			Redirect to next page.
-			*/
+			if (mysqli_num_rows($result) > 0) {
+				$_SESSION["user"] = $emp_username;
+				header("Location: backend/querie_generate.php");
+				// output data of each row
+				while($row = mysqli_fetch_assoc($result)) {
+				/*
+				Redirect to next page.
+				*/
+				}
+			} 
+			else {
+				$data=array('success' => false, 'message' =>'Login Failed try Again');
+				echo '<script> jsfunction(); </script>';
+				
 			}
-		} 
-		else {
-			$data=array('success' => false, 'message' =>'Login Failed try Again');
-			echo '<script type="text/javascript">',
-				'jsfunction();',
-				'</script>';
 		}
 	?>
 
 	<form action="<?php echo $_SERVER["PHP_SELF"];?>" method="POST">
 	   <div class="row">  
-			<div class="col-sm-4"> </div>
+			<div class="col-lg-4 col-sm-4 col-mg-4"> </div>
 				<div class="col-sm-4 loginclass">
 					<img src="image/logo.png" class="imglogin img-responsive center-block"/> 
 				
 					<div class="form-group input-group input-group-lg">
-						<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-						<input type="text" name="emp_username" class="form-control" id="guest_name" placeholder="Guest Name">
+						<span class="input-group-addon"><i class="fa fa-user-o" aria-hidden="true"></i></span>
+						<input type="text" name="emp_username" class="form-control" id="guest_name" placeholder="User Name">
 					</div>
 				
 					<div class="form-group input-group input-group-lg">
-						<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+						<span class="input-group-addon"><i class="fa fa-unlock-alt" aria-hidden="true"></i></span>
 						<input type="password" name="emp_password" class="form-control" id="password" placeholder="Password">
 					</div>
-					<button type="submit" class="btn btn-success center-block">Login</button>
-					<div class="col-sm-offset-5 col-sm-7">
 					
+					<div class="col-sm-12">
+						<button type="submit" class="btn btn-success center-block"><span><i class="fa fa-sign-in" aria-hidden="true"></i></span> Login</button>
 					</div>
-					<div id="login_failed" class="alert alert-danger">
-						<strong>Login Failed !</strong> Please check credentials.
+					<div id="login_failed" class="col-sm-12 alert alert-danger invisible">
+						<strong>Login Failed try Again!</strong> Please check Username and Password....
 					</div>
 			</div> 
 		</div>      
-	</form>   
+	</form>
+	
+	
 	   
 </body>
-	<script type="text/javascript">
-		function jsfunction(){
-			document.getElementById("login_failed").disabled = True;
-		}
-	</script>
+
+	
 
 </html>
