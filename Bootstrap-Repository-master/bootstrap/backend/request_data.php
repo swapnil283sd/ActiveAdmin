1854b30cc1f5c51a7f06f94fe4bec56a1fe9de07
Umesh Charpe <umeshexpert05@gmail.com>
2018-04-03 14:25:42 +0530

    Umesh
    request_data.php

---
+++ Bootstrap-Repository-master/bootstrap/backend/request_data.php
@@ -0,0 +1,18 @@
+<?php
+    header('Content-Type:application/json');
+	
+    include_once 'dbconnect.php';
+    if(!empty($_GET)){
+	   $query_id = $_GET['query_id'];	
+	   $sql = "SELECT * FROM `queries` WHERE `query_id`='$query_id'";
+        $result = mysqli_query($conn, $sql);
+		
+		if (mysqli_num_rows($result) > 0) {
+		    // output data of each row
+		    while($row = mysqli_fetch_assoc($result)) {
+                $data[]=array("emp_name"=>$row['emp_name'], "admin_Name"=>$row['admin_name'], "request"=>$row['request_details'], "Status"=>$row['status'],"query_id"=>$row['query_id'],"date"=>$row['request_date'], "time"=>$row['request_time'] );
+		    }
+			 echo json_encode($data);
+		}
+    }
+?>
