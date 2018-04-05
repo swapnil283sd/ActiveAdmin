f85d561ad1b2f62dcda06368ecf26923b94b2f1d
Swapnil Damkondwar <SD2119@ExpertGS.Com>
2018-04-05 09:03:20 +0530

    SwapnilnewCommit
    commit

--- Bootstrap-Repository-master/bootstrap/backend/request_data.php
+++ Bootstrap-Repository-master/bootstrap/backend/request_data.php
@@ -4,13 +4,19 @@
     include_once 'dbconnect.php';
     if(!empty($_GET)){
 	   $query_id = $_GET['query_id'];	
+
 	   $sql = "SELECT * FROM `queries` WHERE `query_id`='$query_id'";
         $result = mysqli_query($conn, $sql);
-		
+		date_default_timezone_set("Asia/Kolkata");
+
+        $date=date("d-m-y");
+        $time=date("h:i:s");
 		if (mysqli_num_rows($result) > 0) {
 		    // output data of each row
 		    while($row = mysqli_fetch_assoc($result)) {
-                $data[]=array("emp_name"=>$row['emp_name'], "admin_Name"=>$row['admin_name'], "request"=>$row['request_details'], "Status"=>$row['status'],"query_id"=>$row['query_id'],"date"=>$row['request_date'], "time"=>$row['request_time'] );
+
+                $data=array("emp_name"=>$row['emp_name'], "admin_Name"=>$row['admin_name'], "request"=>$row['request_details'], "Status"=>$row['status'],"query_id"=>$row['query_id'],"date"=>$row['request_date'], "time"=>$row['request_time'],"current_date"=>$date, "current_time"=>$time );
+
 		    }
 			 echo json_encode($data);
 		}
--- Bootstrap-Repository-master/bootstrap/myactivity.php
+++ Bootstrap-Repository-master/bootstrap/myactivity.php
@@ -9,12 +9,13 @@
 	<link rel="stylesheet" href="css/backpage.css">
     <link rel="stylesheet" href="css/table.css">
     <link rel="stylesheet" href="css/ticket.css">
+    <script src="js/jquery.min.js"></script>
     <link rel="stylesheet" href="fonts/css/font-awesome.min.css">
 	
     <style>
-        table, th, td {
-            border: 1px solid black !important ;
-            }
+        
+        
+        
     </style>    
 
     <body>
@@ -29,27 +30,17 @@
             <div class="first col-sm-6 "> 
                         <div class="container">
                             
-                                <table class="table" id="querytable">
+                                <table class="table" id="querytable" style=" border: 1px solid black !important ;">
                                         <thead>
-                                                <tr>
-                                                        <th>Query</th>
-                                                        <th>Admin Name</th>
-                                                        <th>Date/Time</th>
-                                                        <th> </th>
+                                                <tr style=" border: 1px solid black !important ;">
+                                                        <th style=" border: 1px solid black !important ;">Ticket No</th>
+                                                        <th style=" border: 1px solid black !important; text-align:center;">Query</th>
+                                                        <th style=" border: 1px solid black !important; text-align:center;">Admin Name</th>
+                                                        <th style=" border: 1px solid black !important; text-align:center;">Date/Time</th>
+                                                        <th style="border: 1px solid black !important; text-align:center; width: 123px;"> </th>
                                                 </tr>
                                         </thead>
-                                    
-                                    <tbody>
-                          
-                                        <tr>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td>
-                                            </td>
-                        
-                                        </tr>
-                                    </tbody>
+                                
                                     
   
                                 </table>
@@ -57,77 +48,95 @@
                             </div>
            </div>    
                              
-                    
+        
                 
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-             
                 <div class="second col-sm-6" >  
                         
                             
                                   
                     <div class="second_upper"   >
                       
-                        <div class="ticketview">
+                        <div class="middle">
+                                <div class="inner">
+                                     <div class="ticketview">
         
-        <div class="ticketheader"> <span id="querytitle"> Visa Management </span></div>
+        <div class="ticketheader"> <span id="querytitle"> Your ticket is empty  </span></div>
         
         
         <div class="upperhalf">
         
-            <span class="tokennumber">Token Number: 1234</span>
-            <span class="timestamp">Date: 28/03/2018</span>
+            <span class="tokennumber" id="tokennumber"></span>
+            <span class="timestamp" id="datestamp"></span>
             <br>
-             <span class="timestamp">Time: 11:43 AM</span>
+             <span class="timestamp" id="timestamp"></span>
             <div class="clearbox"></div>
-            <br>
-            <span class="employeename"><b>Name : Swapnil Damkondwar (10011)</b></span>
+    
+            <span class="employeename" id="employeename"><b></b></span>
 
         
         </div>
     
            
         <div class="cutouts_left"></div>
-        <div class="statusdiv"> <i class="fa fa-trash-o" aria-hidden="true"></i> </div>
+        <div class="statusdiv" > <i class="" id="statusicon" aria-hidden="true"></i> </div>
         <div class="cutouts_right"></div>
         
         
         <div class="lowerhalf">
         
               <br>
-            <span class="employeename"><b>Assigned Person : Person One (10011)</b></span>
+            <span class="employeename" id="assignedperson"><b></b></span>
             <br>
-             <span class="note"><b>" Current Issue Is Deleted By Admin "</b></span>
+             <span class="note" id="note"><b></b></span>
             
             <div class="ticketfooter">
             
-            <span class="tokennumber">Current Date: 28/03/2018</span>
-            <span class="timestamp">Current Time: 07:30 PM </span>
+            <span class="tokennumber" id="downloaddate"></span>
+            <span class="timestamp" id="downloadtime"> </span>
                 
             </div>
         
         </div>
     
+                                </div>
+                        </div>
+                        
+                        
+                        
+                      
     </div>
                         
                     </div>
                         
                             
                             
-                    <div class="second_lower "  >
+                    <div class="second_lower">
                     
-                       
-           
-                        
+                        <div class="middle">
+                                <div class="inner">
+                                    
+                                    <div class="graphicaldiv" style=" background: #ff4c4c;">
+                                      <i class="fa fa-hand-o-up" aria-hidden="true"></i><br>
+                                               1111  
+                                            <span>Raised</span>
+                                    </div>
+                                    
+                                    
+                                    <div class="graphicaldiv" style=" background-color: #1480BF;">
+                                     <i class="fa fa-cogs" aria-hidden="true"></i><br>
+                                               111  
+                                            <span>Inprocess</span>
+                                    </div>
+                                    
+                                    
+                                    <div class="graphicaldiv" style=" background-color: #5FB563;">
+                                        <i class="fa fa-check" aria-hidden="true"></i><br>
+                                               1  
+                                        <span>Completed</span>
+                                    </div>
+                                    
+                                </div>
+                        </div>
                         
                     </div>    
                     
@@ -143,7 +152,7 @@
                 xmlhttprequest.onreadystatechange=function(){
                     
                     if(this.readyState==4 && this.status==200){
-                       var myArray=JSON.parse(this.responseText)
+                       var myArray=JSON.parse(this.responseText);
                        
                        
                         
@@ -153,39 +162,36 @@
                         var table = document.getElementById("querytable");
                         
                         for(var i=0;i<myArray.length;i++){
-                            console.log(i);
+                           
                             var myObject=myArray[i];
                              var row = table.insertRow(1);
                             
-                        row.className='info';
+                    
                             
-                            if(myObject.status== '1'){
+                            if(myObject.status== '2'){
                                  row.className='success';
-                            }else if(myObject.status== '2'){
+                            }else if(myObject.status== '1'){
                                   row.className='warning';
-                            }else if(myObject.status== '3') {
+                            }else if(myObject.status== '0') {
                                   row.className='danger';
+                            }else{
+                                 row.className='info';
                             }
                             
-                        var cell1 = row.insertCell(0);
+                        var cell1 = row.insertCell(0);    
                         var cell2 = row.insertCell(1);
                         var cell3 = row.insertCell(2);
                         var cell4 = row.insertCell(3);
-
-                        cell1.innerHTML = myObject.request;
-                        cell2.innerHTML = myObject.admin_name;
-                        cell3.innerHTML = myObject.date_time;
+                        var cell5 = row.insertCell(4);
+                
+                        cell1.innerHTML=myObject.query_id;    
+                        cell2.innerHTML = myObject.request;
+                        cell3.innerHTML = myObject.admin_name;
+                        cell4.innerHTML = myObject.date_time;
                             
-                        cell4.innerHTML = '';
+                        cell5.innerHTML = '<input type="button" class="btn btn-primary" value="Show" id='+myObject.query_id + ' onclick="myFunction(\''+myObject.query_id+'\')"'+'>'+'&nbsp;<button class="btn btn-success"  id='+myObject.query_id + ' onclick="calleditPage(\''+myObject.query_id+'\')"'+'><i class="fa fa-pencil" aria-hidden="true"></i></button>';
 
-                             
-                            var btn = document.createElement('input');
-                            btn.type = "button";
-                            btn.className = "btn btn-primary";
-                            btn.value="Show";
-                            btn.onclick="getTicketData()";
-                            cell4.appendChild(btn);
-                            
+                        
                         }
                        
                     }
@@ -195,9 +201,73 @@
                 xmlhttprequest.send();
    
             
-                function getTicketData(){
-                    alert();
+function myFunction(mystring) {
+   
+    
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function(){
+        if(this.readyState==4 && this.status==200){
+            
+          
+            var myObject=JSON.parse(this.responseText);
+            
+             
+            document.getElementById("querytitle").innerHTML=myObject.request;
+            document.getElementById("tokennumber").innerHTML="Token Number:- "+myObject.query_id;
+            document.getElementById("timestamp").innerHTML="Time:- "+myObject.time;
+            document.getElementById("datestamp").innerHTML="Date:- "+myObject.date;
+            document.getElementById("employeename").innerHTML="Employee Name:- "+myObject.emp_name;
+             document.getElementById("assignedperson").innerHTML="Assigned Person:- "+myObject.admin_Name;
+             document.getElementById("downloaddate").innerHTML="Download Date:- "+myObject.current_date;
+             document.getElementById("downloadtime").innerHTML="Download Time:- "+myObject.current_time;
+
+            if(myObject.Status==0){
+               
+                document.getElementById("statusicon").className="fa fa-hand-o-up";
+                document.getElementById("note").innerHTML="This task is Raised but not Acknoledged by Admin";    
+                document.getElementById("note").style.color = "#FD4C50";    
+                document.getElementById("statusicon").style.color = "#FD4C50";
+               }
+               else if(myObject.Status==1){
+                
+                    document.getElementById("statusicon").className="fa fa-cog fa-spin  fa-fw";
+                    document.getElementById("note").innerHTML="This task is Inprocess";   
+                    document.getElementById("note").style.color = "#1E81BB";  
+                    document.getElementById("statusicon").style.color = "#1E81BB";
+                
+                }else if(myObject.Status==2){
+                
+                    document.getElementById("statusicon").className="fa fa-check";
+                    document.getElementById("note").innerHTML="This task is Completed";
+                    document.getElementById("note").style.color = "#62B368";
+                    document.getElementById("statusicon").style.color = "#62B368";
+                    document.getElementById("statusdiv").style.borderColor  = "#62B368 ";
+                    
+                    
+                }else{
+                    
+                     document.getElementById("statusicon").className="fa fa-trash";
+                     document.getElementById("note").innerHTML="This task is Deleted";
+                     document.getElementById("note").style.color = "#FD4C50"; 
+                     document.getElementById("statusicon").style.color = "#1E81BB"; 
+                     document.getElementById("statusdiv").style.borderColor = "#1480BF ";
+                      
+                      
                 }
+            
+        }
+    }
+    
+    xmlhttp.open("GET","backend/request_data.php?query_id="+mystring,true);
+    xmlhttp.send();
+    
+    
+    
+}
+
+           function calleditPage(queryId){
+                window.location='http://localhost/updatehotel.php?query_id='+queryId;
+           }
                 
             </script>  
 
