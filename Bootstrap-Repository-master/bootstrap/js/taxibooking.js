04cbf239dbe79b3bc803632061bf12ca99a55140
Umesh Charpe <umeshexpert05@gmail.com>
2018-04-05 11:41:15 +0530

    umesh

--- Bootstrap-Repository-master/bootstrap/js/taxibooking.js
+++ Bootstrap-Repository-master/bootstrap/js/taxibooking.js
@@ -10,3 +10,7 @@ $(document).ready(function () {
         })
 
 
+$(document).ready(function() {
+              $('.dropdown').dropdown();
+              
+          });
--- Bootstrap-Repository-master/bootstrap/updatetaxi.php
+++ Bootstrap-Repository-master/bootstrap/updatetaxi.php
@@ -14,12 +14,20 @@ session_start();
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/taxibooking.js"></script>
+    <link rel="stylesheet" href="css/semantic.min.css"/>
+    <script src="js/semantic.min.js"></script>
     <link rel="stylesheet" href="fonts/css/font-awesome.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Quicksand|Roboto" rel="stylesheet">
     <script type="text/javascript" src="js/datepicker.js"></script>
     <link rel="stylesheet" href="css/datepickerstyle.css" />
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.css"/>
+    <script>
+          $(document).ready(function() {
+              $('ui.dropdown').dropdown();
+              
+          });
+    </script> 
 </head>
 
 <body>
@@ -56,6 +64,19 @@ session_start();
                     </div>
                 </div>    
             </div>
+        
+                <div class="dropdowndiv ui selection dropdown">
+				<input type="hidden" id="admin_name" name="admin_name" required>
+  				<i class="dropdown icon"></i>
+  				<div class="default text">Admin Name</div>
+				<select class="ui fluid dropdown">
+					<option value="">Admin Name</option>
+					<option value="Omkar Deshpande">Omkar Deshpande</option>
+					<option value="Vaidya Sir">Vaidya Sir</option>
+					<option value="Sachin Kulkarni">Sachin Kulkarni</option>
+				</select>
+            </div><br><br>
+            
             <div class="row"></div>
             <div class="row">
                 <div class="col-sm-4"></div>
@@ -74,26 +95,9 @@ session_start();
 			 $taxi_guest_name =$_POST['taxi_guest_name'];
 			 $taxi_date =$_POST['taxi_date'];
 			 $taxi_time = $_POST['taxi_time'];
-
-			/* 
-			 date_default_timezone_set("Asia/Kolkata");
-			 $date=date("y-m-d");
-			 */
-            $user = $_SESSION["user"];
-            $query_id = $_SESSION["query"];
+            $admin_name = $_POST['admin_name'];
 			
-            $sqle = "SELECT `emp_name`,`admin_name`,`status` FROM `queries` WHERE `query_id`='$query_id'";
-			$resulte = mysqli_query($conn, $sqle);
 			
-			if (mysqli_num_rows($resulte) > 0) {
-			    // output data of each row
-				
-				while($row = $resulte->fetch_assoc()) {
-                    $emp_name = $row["emp_name"];
-					$admin_name = $row["admin_name"];
-                    $status = $row["status"];
-				}
-                //request token generate
                                
 			    $sqlr="UPDATE `taxi_booking` SET `admin_name`='$admin_name', `querie_id`='$query_id', `taxi_start_date`='$taxi_date', `taxi_guest_name`='$taxi_guest_name', `taxi_time`='$taxi_time' WHERE `querie_id`='$query_id'";
 				$result=mysqli_query($conn,$sqlr);
