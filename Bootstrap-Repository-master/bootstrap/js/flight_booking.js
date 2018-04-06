3dd128c5f1ca1f792351ab95fb1f91dac59e3eea
Swapnil Damkondwar <SD2119@ExpertGS.Com>
2018-04-06 09:12:24 +0530

    commit

--- Bootstrap-Repository-master/bootstrap/js/flight_booking.js
+++ Bootstrap-Repository-master/bootstrap/js/flight_booking.js
@@ -1,27 +1,5 @@
 $(document).ready(function () {
-<<<<<<< HEAD
-    var date_input = $('input[id="startdate"]'); //our date input has the name "date"
-    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-    date_input.datepicker({
-        format: 'dd/mm/yyyy',
-        container: container,
-        todayHighlight: true,
-        autoclose: true,
-    })
-})
 
-
-$(document).ready(function () {
-    var date_input = $('input[id="enddate"]'); //our date input has the name "date"
-    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-    date_input.datepicker({
-        format: 'dd/mm/yyyy',
-        container: container,
-        todayHighlight: true,
-        autoclose: true,
-    })
-})
-=======
             var date_input = $('input[id="startdate"]'); //our date input has the name "date"
             var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
             date_input.datepicker({
@@ -42,7 +20,7 @@ $(document).ready(function () {
                 autoclose: true,
             })
         })
->>>>>>> eb9c1d675fedb92068fffcb3697a9bd6676a1876
+
 
  
           $(document).ready(function() {
